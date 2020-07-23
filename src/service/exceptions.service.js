de2217aebd3b02ada497bcf54df7d5dab55a3a0b
zombieleet <zombieleetnca@gmail.com>
2020-07-24 00:58:06 +0100

    module for proper express error handling

---
+++ src/service/exceptions.service.js
@@ -0,0 +1,20 @@
+class Exceptions extends Error {
+    constructor(message,errorName,errorDetails) {
+	super(message);
+	this.name = errorName;
+	this.errorDetails = errorDetails;
+    }
+}
+
+
+
+export const ExistsException = message => {
+    return new Exceptions(
+	message,
+	"ExistsException",
+	{
+	    status: 409,
+	    message
+	}
+    );
+};
