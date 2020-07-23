5d581fe71196722b0fd4aa3c96b0f5a9139c1a36
zombieleet <zombieleetnca@gmail.com>
2020-07-24 00:58:30 +0100

    decouple excessive logic in register controller to register service

---
+++ src/service/register.service.js
@@ -0,0 +1,60 @@
+import { v4 as uuidv4 } from "uuid";
+
+export class RegisterService {
+
+    constructor(registerDbUtils,Utils) {
+	RegisterService.__DBUTILS = this.registerDbUtils = registerDbUtils;
+	this.utils = Utils;
+    }
+    
+    static async UpdateUserDetails(lookup,updateOperation) {
+	return await RegisterService.__DBUTILS.updateNewUserDetails({
+            criteria: { [lookup[0]]: lookup[1] },
+            data: { ...updateOperation },
+            options: {
+                new: true,
+                fields: {
+                    password: false,
+                    dateOfReg: false,
+                    _id: false,
+                    __v: false,
+                }
+            }
+        });
+    }
+
+    async checkUserExistence(email,username) {
+	return await this.registerDbUtils.checkEmail(email) || await this.registerDbUtils.checkUserName(username);
+    }
+
+    async saveUser({ username, password, email }) {
+	const userId = uuidv4();
+        const hashedPassword = await this.utils.PasswordUtils.HashPassword(password);
+        const result   = await this.registerDbUtils.saveNewUser({
+	    username,
+	    email,
+	    password: hashedPassword
+	});
+	return result;
+    }
+
+    async verifyValidationTokenAndSetVerified(token) {
+	return await RegisterService.UpdateUserDetails(
+	    [ "verificationLink",  token ],
+	    {
+		$unset: { verificationLink: 1 },
+		$set: { verified: true }
+	    }
+	);
+
+    }
+
+    async updateUserAndCompletetReg(values) {
+	return await RegisterService.UpdateUserDetails(
+	    [ "email", values.email ],
+	    {
+		$set: { ...values, completeReg: true }
+	    }
+	);
+    }
+}
