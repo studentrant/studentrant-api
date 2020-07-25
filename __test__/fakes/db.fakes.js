a3c831b1bc6d1c12adcd2909a6dc9b3d7af6c495
zombieleet <zombieleetnca@gmail.com>
2020-07-25 19:42:10 +0100

    mock RegisterDbUtils

--- __test__/fakes/db.fakes.js
+++ __test__/fakes/db.fakes.js
@@ -1,3 +1,12 @@
 export const usersCollection = {
-    findOne: (value) => Promise.resolve(value),  
+    findOne: (value) => Promise.resolve(value),
 };
+
+
+export class RegisterDbUtils {
+    constructor()          {}
+    updateNewUserDetails() {}
+    checkEmail()           {}
+    checkUserName()        {}
+    saveNewUser()          {}
+}
