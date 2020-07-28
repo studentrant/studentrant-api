d2d82ea277823d13b79cc4894583daed81adec4f
zombieleet <zombieleetnca@gmail.com>
2020-07-28 04:12:48 +0100

    update post-rant unit test to account for recent change in rantPoster

--- src/controllers/post-rant.test.mjs
+++ src/controllers/post-rant.test.mjs
@@ -4,10 +4,11 @@ import { req }  from "../../__test__/fakes/req.fake.js";
 import { res }  from "../../__test__/fakes/res.fake.js";
 import  { RantDbUtils, rantsCollection } from "../../__test__/fakes/db.fakes.js";
 import * as nextValue from "../../__test__/fakes/next.fake.js";
+import * as Utils from "../utils/utils.js";
 
 describe("PostRant [Unit]", () => {
     
-    const controller = new PostRant(RantDbUtils,{},rantsCollection);
+    const controller = new PostRant(RantDbUtils,Utils,rantsCollection);
     
     describe("::createRant", () => {
 	
@@ -20,6 +21,7 @@ describe("PostRant [Unit]", () => {
 		tags: [ "twinkle", "star" ],
 		rantId: uuidv4()
 	    };
+	    req.session = { user: { username: "testuseraccount" } };
 	});
 	
 	afterEach(() => {
@@ -28,22 +30,22 @@ describe("PostRant [Unit]", () => {
 	});
 	
 	it("should create rant and return all tags", async () => {    
-	    createRantSpy.and.resolveTo(req.body);
+	    createRantSpy.and.resolveTo({...req.body, rantPoster: req.session.user.username});
 	    const result = JSON.parse(await controller.createRant(req,res,nextValue.next));
 	    expect(result.status).toEqual(201);
-	    expect(result.message).toEqual(req.body);
+	    expect(result.message).toEqual({...req.body, rantPoster: req.session.user.username});
 	    expect(controller.postRantService.createRant).toHaveBeenCalled();
 	    expect(controller.postRantService.createRant).toHaveBeenCalledWith({
+		rantPoster: req.session.user.username,
 		rant: req.body.rant,
 		tags: req.body.tags
 	    });
 	});
 	it("should create a rant with empty tags and return general as member of the tag", async () => {
-	    createRantSpy.and.resolveTo({...req.body, tags: [ "general" ]});
+	    createRantSpy.and.resolveTo({...req.body, tags: [ "general" ], rantPoster: req.session.user.username });
 	    const result = JSON.parse(await controller.createRant(req,res,nextValue.next));
-	    
 	    expect(result.status).toEqual(201);
-	    expect(result.message).toEqual({...req.body, tags: [ "general" ]});
+	    expect(result.message).toEqual({...req.body, tags: [ "general" ], rantPoster: req.session.user.username });
 	});
     });
 });
