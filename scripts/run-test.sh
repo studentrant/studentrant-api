43298e94ed4a0c2ff8380b253f5fd3bfb397c9a4
zombieleet <zombieleetnca@gmail.com>
2020-09-06 00:27:10 +0100

    monitor env variable in github actions

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-
+OLDIFS=${IFS}
+IFS=""
+echo `env`
+IFS=${OLDIFS}
 #docker exec -it studentrant-server-test npm run test:e2e
 docker exec studentrant-server-test npm run test:unit
