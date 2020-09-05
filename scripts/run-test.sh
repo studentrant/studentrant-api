b4538ad8bd50717838b662096282c3604247d368
zombieleet <zombieleetnca@gmail.com>
2020-09-06 00:21:01 +0100

    add .github direction for github action remove -it from docker exec command

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #docker exec -it studentrant-server-test npm run test:e2e
-docker exec -it studentrant-server-test npm run test:unit
+docker exec studentrant-server-test npm run test:unit
--- scripts/runTest.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#docker exec -it studentrant-server-test npm run test:e2e
-docker exec -it studentrant-server-test npm run test:unit
