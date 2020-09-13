f1a4859b22a8c1ccff610ec103b68ecc6180052d
zombieleet <zombieleetnca@gmail.com>
2020-09-14 00:01:03 +0100

    change github action workflow to only use npm test

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 export NODE_ENV=test
 readonly TEST_TO_RUN="${1}"
+
 ifconfig
+
+[[ -n "${CI}" ]] && npm install
+
 [[ ! "${TEST_TO_RUN}" =~ ^(e2e|unit|\s{0,})$ ]] && {
     printf "%s\n" "e2e | unit test are allowed";
     exit 1;
