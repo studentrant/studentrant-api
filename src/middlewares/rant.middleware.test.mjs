a25ded8e2de2f49490b9e8a338e8774da5bb5973
zombieleet <zombieleetnca@gmail.com>
2021-05-01 23:43:53 +0100

    complete test on middlewares

--- src/middlewares/rant.middleware.test.mjs
+++ src/middlewares/rant.middleware.test.mjs
@@ -32,7 +32,7 @@ describe('RantValidators [Unit]', () => {
       }).toThrowError(rantConstants.RANT_LENGTH_NOT_MORE_THAN_TWENTY);
       expect(nextValue.next).not.toHaveBeenCalled();
     });
-    it('should call next without any parameter if the above conditions meet', () => {
+    it('should call next without any parameter if the above conditions me et', () => {
       req.body.rant = 'hello world'.repeat(50);
       RantValidators.VerifyRant(req, res, nextValue.next);
       expect(nextValue.next).toHaveBeenCalled();
@@ -120,5 +120,102 @@ describe('RantValidators [Unit]', () => {
       }).not.toThrowError(rantConstants.RANT_NOT_VALID_LOAD_NUM_REQUEST);
       expect(nextValue.next).toHaveBeenCalled();
     });
+  });
+  describe("::VerifyTrend", () => {
+    it('should throw rant not a valid trend if it does not start with #', () => {
+      req.params.trend = "bb";
+      expect(() => {
+        RantValidators.VerifyTrend(req,res,nextValue.next);
+      }).toThrowError(rantConstants.RANT_NOT_VALID_TREND);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    })
+
+    it('should not throw error when trend starts with #', () => {
+      req.params.trend = "#xxx";
+      expect(() => {
+        RantValidators.VerifyTrend(req,res,nextValue.next)
+      }).not.toThrowError(rantConstants.RANT_NOT_VALID_TREND);
+      expect(nextValue.next).toHaveBeenCalled();
+    });
+  });
+
+  describe("::VerifyReplyRant", () => {
+
+    it('throw error when req.body.replyRant is undefined', () => {
+      req.body.replyRant = undefined;
+      expect(() => {
+        RantValidators.VerifyReplyRant(req,res,nextValue.next)
+      }).toThrowError(rantConstants.RANT_REPLY_UNDEFINED);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    });
+
+    it('throw error when req.body.replyRant is less than 20', () => {
+      req.body.replyRant = "b".repeat(10);
+      expect(() => {
+        RantValidators.VerifyReplyRant(req,res,nextValue.next)
+      }).toThrowError(rantConstants.RANT_REPLY_NOT_MORE_THAN_TWENTY);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    });
+
+    it('should not throw error when req.body.replyRant meets requirements', () => {
+      req.body.replyRant = "b".repeat(21);
+      expect(() => {
+        RantValidators.VerifyReplyRant(req,res,nextValue.next)
+      }).not.toThrowError(rantConstants.RANT_REPLY_NOT_MORE_THAN_TWENTY);
+      expect(nextValue.next).toHaveBeenCalled();
+    });
+  });
+
+  describe("::VerifyReplyRantId", () => {
+    it('should throw reply rant no rant id if replyRantId is undefined', () => {
+      req.params.replyRantId = undefined;
+      expect(() => {
+        RantValidators.VerifyReplyRantId(req,res,nextValue.next)
+      }).toThrowError(rantConstants.RANT_REPLY_NO_RANT_ID);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    });
+    it('should not throw error if replyRantId is defined', () => {
+      req.params.replyRantId = "xxxxxxxxxxx"
+      expect(() => {
+        RantValidators.VerifyReplyRantId(req,res,nextValue.next);
+      }).not.toThrowError(rantConstants.REPLY_RANT_NO_RANT_ID);
+      expect(nextValue.next).toHaveBeenCalled();
+    });
+  });
+
+  describe("::VerifyReplyRantParams", () => {
+    it('should throw error when rantId and parentCommentId is undefined', () => {
+      req.params.rantId = undefined;
+      req.query.parentCommentId = undefined;
+      expect(() => {
+        RantValidators.VerifyReplyRantParams(req,res,nextValue.next)
+      }).toThrowError(rantConstants.REPLY_RANT_NO_PARAMS);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    });
+    it('should throw error when req.query.parentCommentId is defineds and req.params.rantId is undefined', () => {
+      req.query.parentCommentId = "xxxxxxxxxx";
+      req.params.rantId = undefined;
+      expect(() => {
+        RantValidators.VerifyReplyRantParams(req,res,nextValue.next)
+      }).toThrowError(rantConstants.REPLY_RANT_NO_RANT_ID);
+      expect(nextValue.next).not.toHaveBeenCalled();
+    });
+
+    it('should pass if req.params.rantId is defined', () => {
+      req.params.rantId = 'xxxxxxxxxxxx';
+      expect(() => {
+        RantValidators.VerifyReplyRantParams(req,res,nextValue.next)
+      }).not.toThrowError(rantConstants.RANT_ID_IS_UNDEFINED);
+      expect(nextValue.next).toHaveBeenCalled();
+    });
+
+    it('should pass if req.query.parentCommentId and req.params.rantId is deifned ', () => {
+      req.params.rantId = 'xxxxxx';
+      req.query.parentCommentId = 'qqqqqq';
+      expect(() => {
+        RantValidators.VerifyReplyRantParams(req,res,nextValue.next)
+      }).not.toThrowError(rantConstants.RANT_ID_IS_UNDEFINED);
+      expect(nextValue.next).toHaveBeenCalled();
+    })
   });
 });
